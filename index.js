2a1ec65ab8e5275320d7dba5032da04f996644d5
José Daniel Ramos Barquero <josedanielramosbarquero@gmail.com>
2022-07-25 13:12:21 -0600

    agrego el port de heroku

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-'use strict'
+'use strict';
 
 const mongoose = require('mongoose');
 const app = require('./app');
-const port = 3900;
+const PORT = process.env.PORT || 3900;
 //mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/api_rest_blog',{ useNewUrlParser:true, useUnifiedTopology: true })
-    .then(()=>{
-        console.log('La coneccion a la base de datos se a ejecutado bien');
+mongoose
+	.connect('mongodb://localhost:27017/api_rest_blog', {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log('La coneccion a la base de datos se a ejecutado bien');
 
-        //crear servidor y ponerme  a escuchar peticiones htpp
-        app.listen(port,() =>{
-            console.log('Servidor Corriendo en http://localhost:'+port);
-        })
-});
+		//crear servidor y ponerme  a escuchar peticiones htpp
+		app.listen(PORT, () => {
+			console.log('Servidor Corriendo en: ' + PORT);
+		});
+	});
